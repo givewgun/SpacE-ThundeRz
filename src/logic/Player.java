20e162301e04e373632a056be0096b11af530da1
Gun Kaewngarm <aongoong.gk@gmail.com>
2017-11-23 22:30:39 +0700

    Implement Hp bar (in player class)
    Add
    -new drawHpBar in Player class (using linear gradient)

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -5,6 +5,10 @@ import input.CharacterInput;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import sharedObject.IRenderable;
 import sharedObject.RenderableHolder;
 import window.SceneManager;
@@ -13,10 +17,12 @@ public class Player extends CollidableEntity implements IRenderable {
 	private Image playerImage = null;
 	GameLogic gameLogic;
 	private int bulletDelayTick = 0;
+	private double originalHp;
 
 	public Player(GameLogic gameLogic) {
 		// TODO Auto-generated constructor stub
 		super(1000, 20);
+		this.originalHp = this.hp;
 		playerImage = RenderableHolder.ship3; // temporary player ship might be further implement in the future
 
 		this.gameLogic=gameLogic;
@@ -26,7 +32,7 @@ public class Player extends CollidableEntity implements IRenderable {
 			this.height = playerImage.getHeight();
 			// System.out.println(imageWidth + " " + imageHeight);
 			this.x = SceneManager.SCENE_WIDTH / 2 - this.width / 2;
-			this.y = (SceneManager.SCENE_HEIGHT - this.height) - 20;
+			this.y = (SceneManager.SCENE_HEIGHT - this.height) - 60;
 			this.speed = 3;
 			this.side = 1;
 			this.collideDamage = 10; // test
@@ -35,11 +41,49 @@ public class Player extends CollidableEntity implements IRenderable {
 			height = 0;
 		}
 	}
+	
+	private void drawHpBar(GraphicsContext gc) {
+		double percentHp = this.hp/this.originalHp;
+		if(percentHp >= 0.65) {
+			//gc.setFill(Color.LAWNGREEN);
+			LinearGradient linearGrad = new LinearGradient(
+	                0,   // start X 
+	                0,   // start Y
+	                0,   // end X
+	                1, // end Y
+	                true, // proportional
+	                CycleMethod.NO_CYCLE, // cycle colors
+	                // stops
+	                new Stop(0.1f, Color.LAWNGREEN),
+	                new Stop(1.0f, Color.GREEN));
+	        gc.setFill(linearGrad);
+		}
+		else if(percentHp >= 0.25) {
+			LinearGradient linearGrad = new LinearGradient(0,0,0,1,true,CycleMethod.NO_CYCLE,
+	                // stops
+	                new Stop(0.1f, Color.YELLOW),
+	                new Stop(1.0f, Color.RED));
+	        gc.setFill(linearGrad);
+		}
+		else {
+			LinearGradient linearGrad = new LinearGradient(0,0,0,1,true,CycleMethod.NO_CYCLE,
+	                // stops
+	                new Stop(0.1f, Color.RED),
+	                new Stop(1.0f, Color.BLACK));
+	        gc.setFill(linearGrad);
+		}
+		gc.fillRect(SceneManager.SCENE_WIDTH/2 - 200*percentHp, 750, 2*200*percentHp, 20);
+		
+		
+		
+		
+	}
 
 	@Override
 	public void draw(GraphicsContext gc) {
 		// TODO Auto-generated method stub
 		gc.drawImage(playerImage, x, y);
+		drawHpBar(gc);
 
 	}
 
