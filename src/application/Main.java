b9a5a5cbf4a7a50d5532bead5a05b68310bb4c3f
Gun Kaewngarm <aongoong.gk@gmail.com>
2017-11-24 11:27:25 +0700

    add Memory cleaner after exiting program ( In Main class)
    Add
    -Event handler in Main for exiting program through exit button (stop all thread)

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,10 @@ package application;
 import drawing.GameScreen;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import logic.Background;
 import sharedObject.RenderableHolder;
 import window.SceneManager;
@@ -42,6 +45,17 @@ public class Main extends Application {
 				}
 			};
 			animation.start();*/
+			//this will stop all thread / free all memory when we click exit button of windows (X)
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				
+				@Override
+				public void handle(WindowEvent event) {
+					// TODO Auto-generated method stub
+					Platform.exit();
+					System.exit(0);
+				}
+			});
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
