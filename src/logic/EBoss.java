0aad15237be31f43f98fccb1f4bde29967750688
Gun Kaewngarm <aongoong.gk@gmail.com>
2017-12-05 21:00:50 +0700

    add Boss hp bar

--- src/logic/EBoss.java
+++ src/logic/EBoss.java
@@ -1,17 +1,24 @@
 package logic;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
 import sharedObject.RenderableHolder;
 import window.SceneManager;
 
 public class EBoss extends Enemy {
+	
+	private int originalHp;
 	private GameLogic gameLogic;
 	private int bulletDelayTick = 0;
 
 	public EBoss(GameLogic gameLogic) {
 		super(1000, 0.2);
+		this.originalHp = 1000;
 		this.width = RenderableHolder.eBoss.getWidth();
 		this.height = RenderableHolder.eBoss.getHeight();
 		this.visible = true;
@@ -48,6 +55,14 @@ public class EBoss extends Enemy {
 	public void draw(GraphicsContext gc) {
 		// TODO Auto-generated method stub
 		gc.drawImage(RenderableHolder.eBoss, x, y);
+		drawHpBar(gc);
+	}
+	
+	private void drawHpBar(GraphicsContext gc) {
+		double percentHp = this.hp / this.originalHp;
+		gc.setFill(Color.RED);
+		gc.fillRect(this.x + this.width/2 - this.width/4*percentHp , this.y - 20, this.width/2 * percentHp, 10);
+
 	}
 
 	@Override
